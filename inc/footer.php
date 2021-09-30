55176da093de7bf70b0fcd92cca78bcac2558cf1
Nihaal Shetty <67154176+nihalshetty29@users.noreply.github.com>
2021-09-30 12:53:51 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,29 @@
+    <!-- Footer -->
+    <footer class="footer bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Inventory System <?php echo date('Y'); ?></p>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script> -->
+
+    <!-- Datatables script -->
+    <script type="text/javascript" charset="utf8" src="vendor/DataTables/datatables.js"></script>
+    <script type="text/javascript" charset="utf8" src="vendor/DataTables/sumsum.js"></script>
+
+    <!-- Chosen files for select boxes -->
+    <script src="vendor/chosen/chosen.jquery.min.js"></script>
+    <link rel="stylesheet" href="vendor/chosen/chosen.css" />
+
+    <!-- Datepicker JS -->
+    <script src="vendor/datepicker164/js/bootstrap-datepicker.min.js"></script>
+
+    <!-- Bootbox JS -->
+    <script src="vendor/bootbox/bootbox.min.js"></script>
+
+    <!-- Custom scripts -->
+    <script src="js/scripts.js"></script>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Store Management System: Login</title>
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
+  <link href="./styles/style.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div id="container">
+    <div class="form-wrap">
+      <h1>Store Management System</h1>
+      <form>
+        <p>Select Your Role:</p>
+        <button type="button" id="customer-login" class="btn">Customer</button></a>
+        <button type="button" id="admin-login" class="btn">Admin</button></a>
+      </form>
+    </div>
+  </div>
+  <script src="./js/scripts.js"></script>
+</body>
+
+</html>
